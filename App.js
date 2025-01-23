b6aa9d72a05bc8a3cfbea0c6b7da0850e778e16b
shivalr03 <shivalr03@gmail.com>
2025-01-23 05:37:21 +0530

    package manager added to project again

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-const heading = React.createElement('h1',{id:'heading'},"Hello World I am a React");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const heading = React.createElement('h1',{id:'heading'},"Hello World I am a React🚀");
 const heading2 = React.createElement('h1',{id:'heading'},"Hello World I am a JS");
 const body = React.createElement('div',{},[heading,heading2])
 const root = ReactDOM.createRoot(document.getElementById('root'));
